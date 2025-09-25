1650f1ee31aa0e8e68c320bfa74ef05c56af982f
yeonjoo yoo <yyj.enliple@gmail.com>
2025-09-26 08:03:03 +0900

    1 개 채널 접속

--- app/components/SendbirdChat.tsx
+++ app/components/SendbirdChat.tsx
@@ -1,20 +1,27 @@
 "use client";
 
-import { App as SendbirdApp } from '@sendbird/uikit-react';
 import '@sendbird/uikit-react/dist/index.css';
+
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
+import { GroupChannel } from '@sendbird/uikit-react/GroupChannel';
 
 interface SendbirdChatProps {
   appId: string;
   userId: string;
 }
 
+const channelUrl = 'sendbird_group_channel_25534_3d779482024a78e1a18c6afb60a0dc227cdb78dc';
+
 export default function SendbirdChat({ appId, userId }: SendbirdChatProps) {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <SendbirdApp
+      <SendbirdProvider
         appId={appId}
         userId={userId}
-      />
+      >
+        {/* GroupChannel should be always wrapped inside SendbirdProvider */}
+        <GroupChannel channelUrl={channelUrl} />
+      </SendbirdProvider>
     </div>
   );
 }
